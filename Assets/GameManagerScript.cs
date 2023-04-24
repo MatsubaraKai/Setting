868dc9b8689f1fe26ab222d0ef3aebb494ae74f4
MatsubaraKai <k022g0104@g.neec.ac.jp>
2023-04-24 10:28:23 +0900

    プレイヤーの操作

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Timeline;
 using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
@@ -8,12 +9,20 @@ public class GameManagerScript : MonoBehaviour
     //配列の宣言
     int[] map;
 
+    void PrintArray()
+    {
+        string debugText = "";
+        for(int i=0; i < map.Length; i++)
+        {
+            debugText += map[i].ToString() + ",";
+        }
+        Debug.Log(debugText);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        //配列の実態の作製と初期化
-        map = new int[] {0,0,0,1,0,0,0,0,0,0};
-        //デバックログの出力
+        map = new int[] { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 };
+        PrintArray();
         //Debug.Log("Hello World");
         //for(int i = 0; i < map.Length; i++)
         //{
@@ -21,14 +30,7 @@ public class GameManagerScript : MonoBehaviour
         //}
 
         //追加。文字列の宣言と初期化
-        string debugText = "";
-        for (int i = 0; i < map.Length; i++)
-        {
-            //変更。文字列に結合していく
-            debugText += map[i].ToString() + ",";
-        }
-        //結合した文字列を出力
-        Debug.Log(debugText);
+      
     }
 
     // Update is called once per frame
@@ -36,8 +38,10 @@ public class GameManagerScript : MonoBehaviour
     {
         //見つからなかった時の為に-1で初期化
         int playerIndex = -1;
+        
+        //デバックログの出力
         //要素数はmap.Lengthで取得
-        for (int i = 0; i < map.Length; i++)
+        for (int i = 0; i < map.Length; ++i)
         {
             if (map[i] == 1)
             {
@@ -72,7 +76,7 @@ public class GameManagerScript : MonoBehaviour
             //playerIndex+1のインデックスの物と交換するので、
             //playerIndex-1よりさらに小さいインデックスの時
             //のみ交換処理を行う
-            if (playerIndex < map.Length + 1)
+            if (playerIndex > 0)
             {
                 map[playerIndex - 1] = 1;
                 map[playerIndex] = 0;
